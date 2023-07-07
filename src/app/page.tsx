4cc79a3a3f611e2670ebc3332c2518d00d05c283
Pedro Schmitz <pedroschmitz0000@gmail.com>
2023-07-07 16:27:32 -0300

    feat-test new-button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import styles from './page.module.css'
+import Button from '@/components/Button/Button'
 
 export default function homePage() {
   return (
     <main className={styles.main}>
     <div>
       <h1>STUUKY</h1>
+      <Button name='btn' value='Cadastrar'/>
     </div>
     </main>
   )
