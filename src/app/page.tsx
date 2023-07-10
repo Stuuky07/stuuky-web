0a7e728d9915852749c7ba069f6085caaa6a7380
Pedro Schmitz <88664287+schmitz014@users.noreply.github.com>
2023-07-10 20:15:26 -0300

    New styles added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function homePage() {
   return (
     <main className={styles.main}>
       <div>
-        <h1>STUUKY</h1>
-        <Button name='btn' id='btn' value='Entrar'/>
+        <Title name='title' h1='STUUKY'/>
+        <Button name='button' value='Entrar'/>
       </div>
     </main>
   );
