d780aa7d7ef0ac74bccc0076d4f3ce1e397425d1
Pedro Schmitz <88664287+schmitz014@users.noreply.github.com>
2023-07-10 20:24:31 -0300

    Modified input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function homePage() {
   return (
     <main className={styles.main}>
       <div>
-        <h1>STUUKY</h1>
+        <Title name='title' id='title' h1='STUUKY'/>
         <Button name='btn' id='btn' value='Próximo'/>
       </div>
     </main>
